9c78eb926ee688b2f66dfa6aca8b677b2d37b889
Galina <slovotok@yandex.ru>
2024-04-08 19:09:50 +0300

    правки каталога

--- page-templates/page-catalog.php
+++ page-templates/page-catalog.php
@@ -24,8 +24,183 @@ get_header();
         <div class="row">
           <div class="<?= bootscore_main_col_class(); ?>">
             <!-- Grid Layout -->
+            <div class="d-flex justify-content-between align-items-center mb-4">
+              <h2 class="display-5 fw-bold text-primary">Комплектующие</h2>
+              <div class="mrk-number-products">
+                <span>349 893</span>
+                товаров
+              </div>
+            </div>
+            
             <div class="row">
-              <h2 class="display-5 fw-bold text-primary mb-4">Комплектующие</h2>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Труба нержавеющая</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Круглые трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Уплотнители для поручня с пазом</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручень с пазом под стекло</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Профильные трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Перфорированные трубы</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Комплектующие для перил</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручни и ригели для перил</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Готовые стойки и балясины</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Наконечники стоек и кронштейны для поручня</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Отводы, повороты, соединители поручней и труб</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стеклодержатели</a></li>
+                  <li>
+                    <div class="collapse" id="more-collapse">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Рутели (стеклодержатели)</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплектующие для козырьков на ваннах</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Тяга для козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стекло для навеса</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплекты готовых козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расходные материалы для монтажа козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расчет стоимости стеклянных козырьков</a></li>
+                      </ul>
+                    </div>
+                    <button class="btn btn-toggle d-inline-flex align-items-center border-0 collapsed text-end text-primary fs-6" data-bs-toggle="collapse" data-bs-target="#more-collapse" aria-expanded="false">
+                    Ещё
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Зажимной профиль для стекла</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль Т100</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль k601</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">П-образный профиль</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Комплектующие для перил</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручни и ригели для перил</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Готовые стойки и балясины</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Наконечники стоек и кронштейны для поручня</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Отводы, повороты, соединители поручней и труб</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стеклодержатели</a></li>
+                  <li>
+                    <div class="collapse" id="more-collapse2">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Рутели (стеклодержатели)</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплектующие для козырьков на ваннах</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Тяга для козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стекло для навеса</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплекты готовых козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расходные материалы для монтажа козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расчет стоимости стеклянных козырьков</a></li>
+                      </ul>
+                    </div>
+                    <button class="btn btn-toggle d-inline-flex align-items-center border-0 collapsed text-end text-primary fs-6" data-bs-toggle="collapse" data-bs-target="#more-collapse2" aria-expanded="false">
+                    Ещё
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Зажимной профиль для стекла</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль Т100</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль k601</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">П-образный профиль</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Труба нержавеющая</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Круглые трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Уплотнители для поручня с пазом</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручень с пазом под стекло</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Профильные трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Перфорированные трубы</a></li>
+                  <li>
+                    <div class="collapse" id="more-collapse3">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Рутели (стеклодержатели)</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплектующие для козырьков на ваннах</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Тяга для козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стекло для навеса</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплекты готовых козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расходные материалы для монтажа козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расчет стоимости стеклянных козырьков</a></li>
+                      </ul>
+                    </div>
+                    <button class="btn btn-toggle d-inline-flex align-items-center border-0 collapsed text-end text-primary fs-6" data-bs-toggle="collapse" data-bs-target="#more-collapse3" aria-expanded="false">
+                    Ещё
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Зажимной профиль для стекла</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль Т100</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Алюминиевый профиль k601</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">П-образный профиль</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Комплектующие для перил</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручни и ригели для перил</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Готовые стойки и балясины</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Наконечники стоек и кронштейны для поручня</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Отводы, повороты, соединители поручней и труб</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стеклодержатели</a></li>
+                  <li>
+                    <div class="collapse" id="more-collapse4">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Рутели (стеклодержатели)</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплектующие для козырьков на ваннах</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Тяга для козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стекло для навеса</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплекты готовых козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расходные материалы для монтажа козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расчет стоимости стеклянных козырьков</a></li>
+                      </ul>
+                    </div>
+                    <button class="btn btn-toggle d-inline-flex align-items-center border-0 collapsed text-end text-primary fs-6" data-bs-toggle="collapse" data-bs-target="#more-collapse4" aria-expanded="false">
+                    Ещё
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-4">
+                <h3 class="fs-6 text-uppercase">Труба нержавеющая</h3>
+                <ul class="mrk-list-catalog">
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Круглые трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Уплотнители для поручня с пазом</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Поручень с пазом под стекло</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Профильные трубы</a></li>
+                  <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Перфорированные трубы</a></li>
+                  <li>
+                    <div class="collapse" id="more-collapse5">
+                      <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Рутели (стеклодержатели)</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплектующие для козырьков на ваннах</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Тяга для козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Стекло для навеса</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Комплекты готовых козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расходные материалы для монтажа козырьков</a></li>
+                        <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Расчет стоимости стеклянных козырьков</a></li>
+                      </ul>
+                    </div>
+                    <button class="btn btn-toggle d-inline-flex align-items-center border-0 collapsed text-end text-primary fs-6" data-bs-toggle="collapse" data-bs-target="#more-collapse5" aria-expanded="false">
+                    Ещё
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center mb-4">
+              <h2 class="display-5 fw-bold text-primary">Конструкции с монтажем</h2>
               <div class="mrk-number-products">
                 <span>349 893</span>
                 товаров
