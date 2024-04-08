e70b650e1f75881b8970b9dea6107059d47ed1d5
Galina <slovotok@yandex.ru>
2024-04-08 15:14:31 +0300

    создание страницы catalog

--- page-templates/page-catalog.php
+++ page-templates/page-catalog.php
@@ -1,18 +1,30 @@
 <?php
+
+/**
+ * 
+ * Template Name: Catalog Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bootscore
+ */
+
 get_header();
 ?>
 
-  <div id="content" class="site-content <?= bootscore_container_class(); ?> pb-5 mt-md-2 mt-4">
+<div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-4">
     <div id="primary" class="content-area">
+
       <!-- Hook to add something nice -->
       <?php bs_after_primary(); ?>
+
       <main id="main" class="site-main">
-        
+
+        <!-- Post List -->
         <div class="row">
-          <!-- sidebar -->
-          <?php get_sidebar(); ?>
-          <!-- row -->
-          <div class="<?= bootscore_main_col_class(); ?> position-relative">
+          <div class="<?= bootscore_main_col_class(); ?> px-0">
+            <!-- Grid Layout -->
+                
             <h2 class="text-primary">Комплектующие</h2>
             <div class="row">
               <div class="col-6 col-md-4">
@@ -46,10 +58,17 @@ get_header();
                 </ul>
               </div>
             </div>
+
           </div>
+          <!-- col -->
+          <?php get_sidebar(); ?>
         </div>
+        <!-- row -->
+        
       </main><!-- #main -->
+
     </div><!-- #primary -->
   </div><!-- #content -->
+
 <?php
 get_footer();
