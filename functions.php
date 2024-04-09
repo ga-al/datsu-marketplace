e0d07a33c16a727523a77621f3ee2fef6c75056e
Galina <slovotok@yandex.ru>
2024-04-09 22:09:11 +0300

    обновление крошек

--- functions.php
+++ functions.php
@@ -202,22 +202,29 @@ function custom_search_query( $search ) {
     return $search;
 }
 
-if (!is_home()) {
-  echo '<nav aria-label="breadcrumb" class="breadcrumb-scroller mb-4 mt-2 py-2 px-3">';
-  echo '<ol class="breadcrumb mb-0">';
-  echo '<li class="breadcrumb-item"><a href="' . home_url() . '">' . get_the_title() . '</a></li>';
-  // display parent category names with links
-  if (is_category() || is_single()) {
-    $cat_IDs = wp_get_post_categories(get_the_ID());
-    foreach ($cat_IDs as $cat_ID) {
-      $cat = get_category($cat_ID);
-      echo '<li class="breadcrumb-item"><a href="' . get_term_link($cat->term_id) . '">' . $cat->name . '</a></li>';
+if (!function_exists('the_breadcrumb')) :
+  function the_breadcrumb() {
+
+    if (!is_home()) {
+      echo '<nav aria-label="breadcrumb" class="breadcrumb-scroller mb-4 mt-2 py-2 px-3">';
+      echo '<ol class="breadcrumb mb-0">';
+      echo '<li class="breadcrumb-item"><a href="' . home_url() . '">' . get_the_title() . '</a></li>';
+      // display parent category names with links
+      if (is_category() || is_single()) {
+        $cat_IDs = wp_get_post_categories(get_the_ID());
+        foreach ($cat_IDs as $cat_ID) {
+          $cat = get_category($cat_ID);
+          echo '<li class="breadcrumb-item"><a href="' . get_term_link($cat->term_id) . '">' . $cat->name . '</a></li>';
+        }
+      }
+      // display current page name
+      if (is_page() || is_single()) {
+        echo '<li class="breadcrumb-item active" aria-current="page">' . get_the_title() . '</li>';
+      }
+      echo '</ol>';
+      echo '</nav>';
     }
   }
-  // display current page name
-  if (is_page() || is_single()) {
-    echo '<li class="breadcrumb-item active" aria-current="page">' . get_the_title() . '</li>';
-  }
-  echo '</ol>';
-  echo '</nav>';
-}
+
+  add_filter('breadcrumbs', 'breadcrumbs');
+endif;
