c0e14ab89bed75038138f7ab08a5717f4aae34cc
EvgenyMX <mixalev10a@gmail.com>
2024-04-28 12:46:12 +0300

    edit

--- functions.php
+++ functions.php
@@ -170,150 +170,150 @@ endif;
 /**
  * WooCommerce Breadcrumb
  */
-if (!function_exists('bs_woocommerce_breadcrumbs')) :
-  add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
-  function bs_woocommerce_breadcrumbs() {
-    return array(
-      'delimiter'   => '',
-      'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb breadcrumb-scroller pb-2 px-xxl-1'>
-      <ol class='breadcrumb mb-0'>",
-      'wrap_after'  => '</ol>
-      </nav>',
-      'before'      => '<li class="breadcrumb-item">',
-      'after'       => '</li>',
-      // Remove "Home" and add Fontawesome house icon (_wc_breadcrumb.scss)
-      'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
-      // 'home'        => ' ',
-    );
-  }
-endif;
+// if (!function_exists('bs_woocommerce_breadcrumbs')) :
+//   add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
+//   function bs_woocommerce_breadcrumbs() {
+//     return array(
+//       'delimiter'   => '',
+//       'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb breadcrumb-scroller pb-2 px-xxl-1'>
+//       <ol class='breadcrumb mb-0'>",
+//       'wrap_after'  => '</ol>
+//       </nav>',
+//       'before'      => '<li class="breadcrumb-item">',
+//       'after'       => '</li>',
+//       // Remove "Home" and add Fontawesome house icon (_wc_breadcrumb.scss)
+//       'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
+//       // 'home'        => ' ',
+//     );
+//   }
+// endif;
 
-if (!function_exists('bootscore_container_class')) {
-  function bootscore_container_class() {
-    return "container-xxl";
-  }
-}
+// if (!function_exists('bootscore_container_class')) {
+//   function bootscore_container_class() {
+//     return "container-xxl";
+//   }
+// }
 
-// Пример использования хука pre_get_search_query
-add_filter( 'pre_get_search_query', 'custom_search_query' );
-function custom_search_query( $search ) {
-    // Изменяем содержимое переменной запроса поиска
-    $search = 'custom search query';
-    return $search;
-}
+// // Пример использования хука pre_get_search_query
+// add_filter( 'pre_get_search_query', 'custom_search_query' );
+// function custom_search_query( $search ) {
+//     // Изменяем содержимое переменной запроса поиска
+//     $search = 'custom search query';
+//     return $search;
+// }
 
-// Выводим текст для главной страницы в хлебных крошках
-add_filter('woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text');
-function wcc_change_breadcrumb_home_text($defaults) {
-  $defaults['home'] = _x('Home', 'breadcrumb', 'woocommerce');
+// // Выводим текст для главной страницы в хлебных крошках
+// add_filter('woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text');
+// function wcc_change_breadcrumb_home_text($defaults) {
+//   $defaults['home'] = _x('Home', 'breadcrumb', 'woocommerce');
 
-  return $defaults;
-}
+//   return $defaults;
+// }
 
-add_action( 'woocommerce_product_options_advanced', 'rudr_product_field' );
-function rudr_product_field(){
+// add_action( 'woocommerce_product_options_advanced', 'rudr_product_field' );
+// function rudr_product_field(){
 
-	echo '<div class="options_group">';
-	woocommerce_wp_text_input(
-		array(
-			'id'      => 'product_link_video',
-			'value'   => get_post_meta( get_the_ID(), 'product_link_video', true ),
-			'label'   => 'Сылка видео',
-			'desc_tip' => true,
-			'description' => 'Вставьте сылку на видео',
-		)
-	);
-	echo '</div>';
+// 	echo '<div class="options_group">';
+// 	woocommerce_wp_text_input(
+// 		array(
+// 			'id'      => 'product_link_video',
+// 			'value'   => get_post_meta( get_the_ID(), 'product_link_video', true ),
+// 			'label'   => 'Сылка видео',
+// 			'desc_tip' => true,
+// 			'description' => 'Вставьте сылку на видео',
+// 		)
+// 	);
+// 	echo '</div>';
 
-}
-add_action( 'woocommerce_process_product_meta', 'rudr_save_field' );
-function rudr_save_field( $id ){
+// }
+// add_action( 'woocommerce_process_product_meta', 'rudr_save_field' );
+// function rudr_save_field( $id ){
 
-	$super = isset( $_POST[ 'product_link_video' ] ) ? $_POST[ 'product_link_video' ] : "";
-	update_post_meta( $id, 'product_link_video', $super );
+// 	$super = isset( $_POST[ 'product_link_video' ] ) ? $_POST[ 'product_link_video' ] : "";
+// 	update_post_meta( $id, 'product_link_video', $super );
 
-}
-
-
-// Walker Categoris Sidebar
-require_once('inc/walker-categories-sidebar.php');
-
-// Custom Create Vendor
-function me_create_vendor() {
-  if ( ! class_exists( 'YITH_Vendors_Frontend_Premium') ) return;
-
-}
+// }
 
 
-// Registration Form
+// // Walker Categoris Sidebar
+// require_once('inc/walker-categories-sidebar.php');
 
-add_action( 'woocommerce_register_form_start', 'truemisha_form_registration_fields', 25 );
-function truemisha_form_registration_fields() {
+// // Custom Create Vendor
+// function me_create_vendor() {
+//   if ( ! class_exists( 'YITH_Vendors_Frontend_Premium') ) return;
 
-  echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-        <label for="kind_of_name">Имя<span class="required">*</span></label>
-        <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="billing_first_name" required id="kind_of_name" value="">
-      </p>';
-
-  echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-        <label for="kind_of_l_name">Фамилия<span class="required">*</span></label>
-        <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="billing_last_name" required id="kind_of_l_name" value="">
-      </p>';
+// }
 
 
-  echo '<div class="clear"></div>';
-  echo 'Стать продавцом';
-  echo '<div class="clear"></div>';
-  echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-              <label for="vendor_name">Названия вашей компании</label>
-              <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-name" id="vendor_name" value="">
-        </p>';
-  echo '<div class="clear"></div>';
-  echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-              <label for="vendor-telephone">Телефон</label>
-              <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-telephone" id="vendor-telephone" value="">
-        </p>';
-  echo '<div class="clear"></div>';
-  echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-              <label for="vendor-location">Телефон</label>
-              <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-location" id="vendor-location" value="">
-        </p>';
+// // Registration Form
+
+// add_action( 'woocommerce_register_form_start', 'truemisha_form_registration_fields', 25 );
+// function truemisha_form_registration_fields() {
+
+//   echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+//         <label for="kind_of_name">Имя<span class="required">*</span></label>
+//         <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="billing_first_name" required id="kind_of_name" value="">
+//       </p>';
+
+//   echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+//         <label for="kind_of_l_name">Фамилия<span class="required">*</span></label>
+//         <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="billing_last_name" required id="kind_of_l_name" value="">
+//       </p>';
 
 
-	// чтобы всё не съехало, ведь у нас "на флоатах"
-	echo '<div class="clear"></div>';
+//   echo '<div class="clear"></div>';
+//   echo 'Стать продавцом';
+//   echo '<div class="clear"></div>';
+//   echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+//               <label for="vendor_name">Названия вашей компании</label>
+//               <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-name" id="vendor_name" value="">
+//         </p>';
+//   echo '<div class="clear"></div>';
+//   echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+//               <label for="vendor-telephone">Телефон</label>
+//               <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-telephone" id="vendor-telephone" value="">
+//         </p>';
+//   echo '<div class="clear"></div>';
+//   echo '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+//               <label for="vendor-location">Телефон</label>
+//               <input type="text" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="vendor-location" id="vendor-location" value="">
+//         </p>';
 
-}
 
-add_action( 'woocommerce_created_customer', 'truemisha_save_fields', 25 );
+// 	// чтобы всё не съехало, ведь у нас "на флоатах"
+// 	echo '<div class="clear"></div>';
 
-function truemisha_save_fields( $user_id ) {
+// }
 
-	// сохраняем Имя
-	if ( isset( $_POST[ 'billing_first_name' ] ) ) {
-		update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
-		update_user_meta( $user_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
-	}
-	// сохраняем Фамилию
-	if ( isset( $_POST[ 'billing_last_name' ] ) ) {
-		update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
-		update_user_meta( $user_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
-	}
+// add_action( 'woocommerce_created_customer', 'truemisha_save_fields', 25 );
 
-}
-add_action( 'wp_enqueue_scripts', 'add_data_to_scripts' );
-function add_data_to_scripts() {
-  $userData = wp_get_current_user();
-  $user = $userData->ID ? true : false;
+// function truemisha_save_fields( $user_id ) {
 
-	$data = [];
-	if ( $userData->ID ) {
-		$data['user'] = [
-			'user_id' => $userData->ID,
-			'user_nickname'=>$userData->nickname
-		];
-	} else {
-		$data['user'] = false;
-	}
-    wp_add_inline_script('x1team-main', 'window.x1teamMainData = '.wp_json_encode( $data ), 'before');
-}
+// 	// сохраняем Имя
+// 	if ( isset( $_POST[ 'billing_first_name' ] ) ) {
+// 		update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
+// 		update_user_meta( $user_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
+// 	}
+// 	// сохраняем Фамилию
+// 	if ( isset( $_POST[ 'billing_last_name' ] ) ) {
+// 		update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
+// 		update_user_meta( $user_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
+// 	}
+
+// }
+// add_action( 'wp_enqueue_scripts', 'add_data_to_scripts' );
+// function add_data_to_scripts() {
+//   $userData = wp_get_current_user();
+//   $user = $userData->ID ? true : false;
+
+// 	$data = [];
+// 	if ( $userData->ID ) {
+// 		$data['user'] = [
+// 			'user_id' => $userData->ID,
+// 			'user_nickname'=>$userData->nickname
+// 		];
+// 	} else {
+// 		$data['user'] = false;
+// 	}
+//     wp_add_inline_script('x1team-main', 'window.x1teamMainData = '.wp_json_encode( $data ), 'before');
+// }
