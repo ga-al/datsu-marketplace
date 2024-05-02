0a4f1a550fc54ee2bb7508658dee4d2b4818424e
EvgenyMX <mixalev10a@gmail.com>
2024-05-02 23:42:52 +0300

    edit

--- inc/me-function.php
+++ inc/me-function.php
@@ -97,3 +97,34 @@ function get_current_vendor_id() {
     $product_vendor_id = $product_vendor ? $product_vendor->id : '-';
     return $product_vendor_id;
 }
+
+
+add_filter( 'woocommerce_checkout_fields', 'me_del_fields_checkout' );
+function me_del_fields_checkout( $fields ) {
+
+    foreach ($fields[ 'billing' ] as $key => $value) {
+        $fields[ 'billing' ][$key][ 'required' ] = false;
+    }
+    foreach ($fields[ 'shipping' ] as $key => $value) {
+        $fields[ 'shipping' ][$key][ 'required' ] = false;
+    }
+
+    // оставляем эти поля
+    // unset( $fields[ 'billing' ][ 'billing_first_name' ] ); // имя
+    // unset( $fields[ 'billing' ][ 'billing_last_name' ] ); // фамилия
+    // unset( $fields[ 'billing' ][ 'billing_email' ] ); // емайл
+    var_dump(';12321321');
+    // удаляем все эти поля
+    unset( $fields[ 'billing' ][ 'billing_phone' ] ); // телефон
+    unset( $fields[ 'billing' ][ 'billing_company' ] ); // компания
+    unset( $fields[ 'billing' ][ 'billing_country' ] ); // страна
+    unset( $fields[ 'billing' ][ 'billing_address_1' ] ); // адрес 1
+    unset( $fields[ 'billing' ][ 'billing_address_2' ] ); // адрес 2
+    unset( $fields[ 'billing' ][ 'billing_city' ] ); // город
+    unset( $fields[ 'billing' ][ 'billing_state' ] ); // регион, штат
+    unset( $fields[ 'billing' ][ 'billing_postcode' ] ); // почтовый индекс
+    unset( $fields[ 'order' ][ 'order_comments' ] ); // заметки к заказу
+
+    return $fields;
+
+}
