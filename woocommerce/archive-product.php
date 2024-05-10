8b1dfd1c411ff4ef09b9f1b117c6e047d14cfd17
Galina <slovotok@yandex.ru>
2024-05-10 23:19:11 +0300

    проверка библиотеки масонри

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -52,7 +52,63 @@ if ( isset( $queried_object->term_id ) ) {
 									<?php
 								}
 							?>
+							<div class="row" data-masonry='{"percentPosition": true }'>
+								<?php if (have_posts()) : ?>
+								<?php while (have_posts()) : the_post(); ?>
 
+									<div class="col-md-6 col-lg-4 col-xxl-3 mb-4">
+
+									<div class="card">
+
+										<?php if ( has_post_thumbnail() ) : ?>
+										<a href="<?php the_permalink(); ?>">
+											<?php the_post_thumbnail('medium', array('class' => 'card-img-top')); ?>
+										</a>
+										<?php endif; ?>
+
+										<div class="card-body d-flex flex-column">
+
+										<?php bootscore_category_badge(); ?>
+
+										<a class="text-body text-decoration-none" href="<?php the_permalink(); ?>">
+											<?php the_title('<h2 class="blog-post-title h5">', '</h2>'); ?>
+										</a>
+
+										<?php if ('post' === get_post_type()) : ?>
+											<p class="meta small mb-2 text-body-secondary">
+											<?php
+												bootscore_date();
+												bootscore_author();
+												bootscore_comments();
+												bootscore_edit();
+											?>
+											</p>
+										<?php endif; ?>
+
+										<p class="card-text">
+											<a class="text-body text-decoration-none" href="<?php the_permalink(); ?>">
+											<?= strip_tags(get_the_excerpt()); ?>
+											</a>
+										</p>
+
+										<p class="card-text mt-auto">
+											<a class="read-more" href="<?php the_permalink(); ?>">
+											<?php _e('Read more »', 'bootscore'); ?>
+											</a>
+										</p>
+
+										<?php bootscore_tags(); ?>
+
+										</div>
+
+									</div>
+
+									</div>
+
+								<?php endwhile; ?>
+								<?php endif; ?>
+
+							</div>
 							<nav class="d-flex justify-content-between align-items-center my-3">
 								<div class="nav nav-tabs orderby custom-select d-flex gap-md-5" id="nav-tab" role="tablist" >
 									<!-- СОРТИРОВКА ТОВАРОВ .me_btn_sort_producrs & data-sort & data-term_id !!! -->
