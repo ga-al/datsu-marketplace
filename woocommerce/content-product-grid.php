4469181c8736bc41a0c6c32eceacf1e090056bbc
Galina <slovotok@yandex.ru>
2024-05-17 22:40:13 +0300

    подключение файла

--- woocommerce/content-product-grid.php
+++ woocommerce/content-product-grid.php
@@ -3,7 +3,7 @@
 /**
  * The template for displaying product content within loops
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-product-grid.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
