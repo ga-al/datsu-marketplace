5fea1c017164db4cc9953281a383acd8c7524877
Galina <slovotok@yandex.ru>
2024-05-21 21:49:05 +0300

    обнвление позиции галереи

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -34,8 +34,8 @@ $social_menu = me_render_social();
                 <div class="col">
                   <div class="card-body pt-0 pb-md-4 px-0">
 
-                    <div class="mrk-gallery">
-                      <div class="mrk-gallery-top mx-auto position-relative"><img class="mrk-gallery-image mx-auto h-100 w-100" src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/gallery/image-1.jpg" alt="vidio-image">
+                    <div class="mrk-gallery position-relative">
+                      <div class="mrk-gallery-top mx-auto "><img class="mrk-gallery-image mx-auto h-100 w-100" src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/gallery/image-1.jpg" alt="vidio-image">
                       <div class="mrk-gallery-blur mrk-bg-blur position-absolute start-0 bottom-0">
                         <h1 class="d-inline-block text-uppercase text-primary fw-bold">все для стеклянных козырьков</h1>
                         <a class="btn btn-outline-primary d-flex align-items-center justify-content-center gap-2 text-white text-nowrap mt-2" href="/shop/" type="button">
